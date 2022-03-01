0b38a2a9146436d8667917c2ee9d2d36f57d9f49
clakhlani <chintan.lakhlani@gmail.com>
2022-03-01 19:38:19 +0530

    Commit basic changes

--- eventbooking/urls.py
+++ eventbooking/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home_page,name='home'),
+    path('event/<str:event_id>',views.event_page,name='events'),
+
 ]
--- eventbooking/views.py
+++ eventbooking/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+events=[
+{"id": 1 , "name": "Standup"},
+{"id": 2 , "name": "Sports"},
+{"id": 3 , "name": "Drama"},
+{"id": 4 , "name": "Fair"},
+]
+
 
 def home_page(request):
-	return render(request,'home.html')
+	return render(request,'eventbooking/home.html',{"events":events})
+
+def event_page(request,event_id):
+	event= None
+	for evt in events:
+		print(evt)
+		if evt["id"]==int(event_id):
+			event=evt
+		context={"event":event}
+	return render(request,'eventbooking/events.html',context)
